62361b9aedddad27e5884f2d5ce38388a1b0fe68
NikolaDedic <60113839+NikolaDedic@users.noreply.github.com>
2020-02-03 23:32:34 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+ $(window).scroll(function() {
+            var height = $(window).scrollTop();
+            if (height > 100) {
+                $('#back2Top').fadeIn();
+            } else {
+                $('#back2Top').fadeOut();
+            }
+        });
+
+
+
+       $(document).ready(function() {
+            $("a").on('click', function(event) {
+                if (this.hash !== "") {
+                    event.preventDefault();
+                    var hash = this.hash;
+
+                    $('html, body').animate({
+                        scrollTop: $(hash).offset().top
+                    }, 1300, function() {
+                        window.location.hash = hash;
+                    });
+                }
+            });
+        });
+
+
+ 
+
+	  
+
+  
